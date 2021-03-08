fbfaa98852ab7267c8ccd8adca7b59fc9f74251b
Jonathan Barrow <jonbarrow1998@gmail.com>
2021-03-07 19:28:29 -0500

    Create pretendo_addon.py
    Added Pretendo addon for easier redirection to Pretendo servers

---
+++ pretendo_addon.py
@@ -0,0 +1,30 @@
+from mitmproxy import http, ctx
+
+
+class PretendoAddon:
+	def load(self, loader) -> None:
+		loader.add_option(
+			name='pretendo_redirect',
+			typespec = bool,
+			default = False,
+			help='Redirect all requests from Nintendo to Pretendo'
+		)
+
+		loader.add_option(
+			name='pretendo_http',
+			typespec = bool,
+			default = False,
+			help='Sets Pretendo requests to HTTP'
+		)
+
+	def request(self, flow: http.HTTPFlow) -> None:
+		if ctx.options.pretendo_redirect:
+			if 'nintendo.net' in flow.request.host:
+				flow.request.host = flow.request.host.replace('nintendo.net', 'pretendo.cc')
+			
+			if ctx.options.pretendo_http:
+				flow.request.scheme = 'http'
+
+addons = [
+	PretendoAddon()
+]
