827f6a0af6cb40f3f76126129e68ae655040ff37
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2021-08-30 19:40:53 -0400

    Allow redirecting traffic from multiple servers in the addon
    This replaces the simple "nintendo.net => pretendo.cc" substitution with a customized redirection for each Pretendo server.
    
    This is necessary for several reasons:
     - The Grove server expects `eshop.pretendo.cc`, not `geisha-wup`...
     - Each server must be run on a different port. The port numbers must be configured manually for each Pretendo server.

--- pretendo_addon.py
+++ pretendo_addon.py
@@ -2,25 +2,62 @@ from mitmproxy import http, ctx
 
 
 class PretendoAddon:
-	def load(self, loader) -> None:
-		loader.add_option(
+    def load(self, loader) -> None:
+        loader.add_option(
             name="pretendo_redirect",
             typespec=bool,
             default=False,
             help="Redirect all requests from Nintendo to Pretendo",
-		)
+        )
 
-		loader.add_option(
+        loader.add_option(
             name="pretendo_http",
             typespec=bool,
             default=False,
             help="Sets Pretendo requests to HTTP",
-		)
+        )
 
-	def request(self, flow: http.HTTPFlow) -> None:
-		if ctx.options.pretendo_redirect:
-			if 'nintendo.net' in flow.request.host:
-				flow.request.host = flow.request.host.replace('nintendo.net', 'pretendo.cc')
-			
-			if ctx.options.pretendo_http:
+    def request(self, flow: http.HTTPFlow) -> None:
+        if not ctx.options.pretendo_redirect:
+            # This script should just be disabled
+            return
+
+        # The account server
+        if "account.nintendo.net" in flow.request.host:
+            flow.request.host = flow.request.host.replace("nintendo.net", "pretendo.cc")
+            flow.request.port = 7070
+
+            if ctx.options.pretendo_http:
+                flow.request.scheme = "http"
+
+        # The Grove eShop server
+        if "geisha-wup.cdn.nintendo.net" in flow.request.host:
+            flow.request.host = flow.request.host.replace(
+                "geisha-wup.cdn.nintendo.net", "eshop.pretendo.cc"
+            )
+            flow.request.port = 8081
+
+            if ctx.options.pretendo_http:
+                flow.request.scheme = "http"
+
+        # The olv-api Miiverse API server
+        if (
+            "discovery.olv.nintendo.net" in flow.request.host
+            or "api.olv.nintendo.net" in flow.request.host
+        ):
+            flow.request.host = flow.request.host.replace("nintendo.net", "pretendo.cc")
+            flow.request.port = 8082
+
+            if ctx.options.pretendo_http:
+                flow.request.scheme = "http"
+
+        # The Juxtaposition Miiverse server
+        elif "olv.nintendo.net" in flow.request.host:
+            flow.request.host = flow.request.host.replace("nintendo.net", "pretendo.cc")
+            flow.request.port = 8083
+
+            if ctx.options.pretendo_http:
+                flow.request.scheme = "http"
+
+
 addons = [PretendoAddon()]
