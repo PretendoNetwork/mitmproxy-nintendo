a884ccec304b6d4703a6f22e911a9739d09f4a5c
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2022-07-22 23:34:51 -0400

    Add support for rverse redirection

--- pretendo_addon.py
+++ pretendo_addon.py
@@ -23,6 +23,8 @@ class PretendoAddon:
                 flow.request.host_header = flow.request.pretty_host.replace('nintendo.net', 'pretendo.cc')
             elif 'nintendowifi.net' in flow.request.pretty_host:
                 flow.request.host_header = flow.request.pretty_host.replace('nintendowifi.net', 'pretendo.cc')
+            elif 'rverse.club' in flow.request.pretty_host:
+                flow.request.host_header = flow.request.pretty_host.replace('rverse.club', 'pretendo.cc')
 
             if ctx.options.pretendo_http:
                 flow.request.scheme = 'http'
