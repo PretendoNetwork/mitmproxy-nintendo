25ff8d1cb262c6980932cd404f5d5d2b7e4bf6a6
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-12-16 15:58:00 -0500

    Name the container

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 docker build . -t ghcr.io/matthewl246/mitmproxy-pretendo
-docker run -it --rm -v mitmproxy-pretendo-data:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 ghcr.io/matthewl246/mitmproxy-pretendo mitmweb --web-host 0.0.0.0
+docker run -it --rm --name mitmproxy-pretendo -v mitmproxy-pretendo-data:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 ghcr.io/matthewl246/mitmproxy-pretendo mitmweb --web-host 0.0.0.0
