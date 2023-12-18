2d05ef8c66cec1413d4c1987c0780a23b34cf778
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-12-18 17:42:43 -0500

    Add support for pretendo.network and reformat
    (they hardcoded the website cookie domains)

--- pretendo_addon.py
+++ pretendo_addon.py
@@ -6,7 +6,7 @@ class PretendoAddon:
         loader.add_option(
             name="pretendo_redirect",
             typespec=bool,
-            default=False,
+            default=True,
             help="Redirect all requests from Nintendo to Pretendo",
         )
 
@@ -18,33 +18,41 @@ class PretendoAddon:
         )
 
         loader.add_option(
-            name="pretendo_http",
-            typespec=bool,
-            default=False,
-            help="Sets Pretendo requests to HTTP (only applies if pretendo_host is set)",
-        )
-
-        loader.add_option(
             name="pretendo_host_port",
             typespec=int,
             default=80,
             help="Port to send Pretendo requests to (only applies if pretendo_host is set)",
         )
 
+        loader.add_option(
+            name="pretendo_http",
+            typespec=bool,
+            default=False,
+            help="Sets Pretendo requests to HTTP (only applies if pretendo_host is set)",
+        )
+
     def request(self, flow: http.HTTPFlow) -> None:
         if ctx.options.pretendo_redirect:
-            if 'nintendo.net' in flow.request.pretty_host:
-                flow.request.host = flow.request.pretty_host.replace('nintendo.net', 'pretendo.cc')
-            elif 'nintendowifi.net' in flow.request.pretty_host:
-                flow.request.host = flow.request.pretty_host.replace('nintendowifi.net', 'pretendo.cc')
+            if "nintendo.net" in flow.request.pretty_host:
+                flow.request.host = flow.request.pretty_host.replace(
+                    "nintendo.net", "pretendo.cc"
+                )
+            elif "nintendowifi.net" in flow.request.pretty_host:
+                flow.request.host = flow.request.pretty_host.replace(
+                    "nintendowifi.net", "pretendo.cc"
+                )
 
-            if 'pretendo.cc' in flow.request.pretty_host and ctx.options.pretendo_host:
+            if ctx.options.pretendo_host and (
+                "pretendo.cc" in flow.request.pretty_host
+                or "pretendo.network" in flow.request.pretty_host
+            ):
                 original_host = flow.request.host_header
                 flow.request.host = ctx.options.pretendo_host
                 flow.request.port = ctx.options.pretendo_host_port
                 flow.request.host_header = original_host
 
                 if ctx.options.pretendo_http:
-                    flow.request.scheme = 'http'
+                    flow.request.scheme = "http"
+
 
 addons = [PretendoAddon()]
